b70198b3a916808cbfe95b5fbeb00077f49765fc
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-04-12 20:52:12 +0200

    introduced method for calculation of multiple weighted grades

--- src/main/java/ch/tdd/GradeCalculator.java
+++ src/main/java/ch/tdd/GradeCalculator.java
@@ -21,4 +21,16 @@ public class GradeCalculator {
     public double calculateWeighted(WeightedGrade weightedGrade, WeightedGrade weightedGrade1) {
         return ((weightedGrade.getGrade() * weightedGrade.getCreditPoints()) + (weightedGrade1.getGrade() * weightedGrade1.getCreditPoints())) / (weightedGrade.getCreditPoints() + weightedGrade1.getCreditPoints());
     }
+
+    public double calculateMultipleWeighted(WeightedGrade... weightedGrade) {
+        double result = 0.0;
+        int totalCreditPoints = 0;
+        for (WeightedGrade grade : weightedGrade) {
+            result += grade.getGrade() * grade.getCreditPoints();
+            totalCreditPoints += grade.getCreditPoints();
+        }
+        return result / totalCreditPoints;
+    }
+
+
 }
