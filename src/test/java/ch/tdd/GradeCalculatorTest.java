e7c377c3989c836305d5df19e4837dc376ca2722
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-04-12 21:56:31 +0200

    refactor test cases to arrange act assert

--- src/test/java/ch/tdd/GradeCalculatorTest.java
+++ src/test/java/ch/tdd/GradeCalculatorTest.java
@@ -19,42 +19,54 @@ public class GradeCalculatorTest {
 
     @Test
     public void calculateAverage_onlyOneGrade() {
-        final double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(1.0, 5))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(1.0, 5)));
+
+        final double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         Assert.assertEquals(1, result, 0.0);
     }
 
     @Test
     public void calculateAverage_twoGrades() throws Exception {
-        final double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(2.0, 5), new WeightedGrade(3.0, 5))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(2.0, 5), new WeightedGrade(3.0, 5)));
+
+        final double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         assertEquals(2.5, result, 0.0);
     }
 
     @Test
     public void calculcateAverage_threeGrades() throws Exception {
-        double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(2.0, 5), new WeightedGrade(3.0, 5), new WeightedGrade(5.0, 5))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(2.0, 5), new WeightedGrade(3.0, 5), new WeightedGrade(5.0, 5)));
+
+        double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         assertEquals(3.33, result, 0.01);
     }
 
     @Test
     public void calculateAverage_twoOddGrades() throws Exception {
-        double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(2.3, 5), new WeightedGrade(1.7, 5))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(2.3, 5), new WeightedGrade(1.7, 5)));
+
+        double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         assertEquals(2.0, result, 0.0);
     }
 
     @Test
     public void calculateAverage_weightedGrade() throws Exception {
-        double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(2.3, 8))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(2.3, 8)));
+
+        double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         assertEquals(2.3, result, 0.0);
     }
 
     @Test
     public void calculateAverage_twoWeightedGrades() throws Exception {
-        double result = gradeCalculator.calculateMultipleWeighted(new TableOfRecords(Arrays.asList(new WeightedGrade(4.0, 8), new WeightedGrade(1.0, 4))));
+        TableOfRecords tableOfRecords = new TableOfRecords(Arrays.asList(new WeightedGrade(4.0, 8), new WeightedGrade(1.0, 4)));
+
+        double result = gradeCalculator.calculateMultipleWeighted(tableOfRecords);
 
         assertEquals(3, result, 0.0);
     }
