d0fb2dae9f57119e504fa8368bc12760370f9b8e
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-04-12 20:13:51 +0200

    refactoring: use setUp method

--- src/test/java/ch/tdd/GradeCalculatorTest.java
+++ src/test/java/ch/tdd/GradeCalculatorTest.java
@@ -1,15 +1,21 @@
 package ch.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GradeCalculatorTest {
 
+    private GradeCalculator gradeCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        gradeCalculator = new GradeCalculator();
+    }
+
     @Test
     public void calculateAverage_onlyOneGrade() {
-        final GradeCalculator gradeCalculator = new GradeCalculator();
-
         final int result = gradeCalculator.calculate(1);
 
         assertEquals(1, result);
@@ -17,9 +23,7 @@ public class GradeCalculatorTest {
 
     @Test
     public void calculateAverage_twoGrades() throws Exception {
-        GradeCalculator gradeCalculator = new GradeCalculator();
-
-        double result = gradeCalculator.calculate(2, 3);
+        final double result = gradeCalculator.calculate(2, 3);
 
         assertEquals(2.5, result, 0.0);
     }
