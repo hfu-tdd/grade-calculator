9a24cdc5f10e010d03e7c8979425b30d0a4dc72b
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-04-12 20:27:33 +0200

    cerate generic calculate method

--- src/main/java/ch/tdd/GradeCalculator.java
+++ src/main/java/ch/tdd/GradeCalculator.java
@@ -13,4 +13,12 @@ public class GradeCalculator {
     public double calculate(int i, int i1, int i2) {
         return (double)(i + i1 + i2) / 3;
     }
+
+    public double calculateMultiple(int... grades) {
+        double sumOfGrades = 0.0;
+        for (int grade : grades) {
+            sumOfGrades += grade;
+        }
+        return sumOfGrades / grades.length;
+    }
 }
